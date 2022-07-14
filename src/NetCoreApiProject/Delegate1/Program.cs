4571bb277e06733ae3ef803a447db0ae3ea8ad80
semih.canpolat@vakifkatilim.com.tr <semih.canpolat@vakifkatilim.com.tr>
2022-07-14 08:51:27 +0300

    added delegate

---
+++ src/NetCoreApiProject/Delegate1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _01.Delegate
+{
+    class Program
+    {
+        delegate void Islemler(int num1, int num2);
+
+        static void Main(string[] args)
+        {
+            Islemler delegateIslemler = new Islemler(Topla);
+            delegateIslemler.Invoke(2, 3);
+            delegateIslemler = delegateIslemler + Carp;
+            delegateIslemler.Invoke(2, 3);
+            Console.ReadKey();
+        }
+
+        static void Topla(int num1, int num2)
+        {
+            Console.WriteLine(num1 + num2);
+        }
+
+        static void Carp(int num1, int num2)
+        {
+            Console.WriteLine(num1 * num2);
+        }
+    }
+}
---
+++ src/NetCoreApiProject/_03.ActionDelegates/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _03.ActionDelegates
+{
+    class Program
+    {
+        //func,action,predicate
+
+        static void Main(string[] args)
+        {
+            Action action = new Action(SelamVer);
+            action.Invoke();
+
+            Action<string> action2 = new Action<string>(SelamVer2);
+            action2.Invoke("Ali");
+
+            //2.action delege calistirma sekli
+            Action<string> action3 = new Action<string>(obj =>
+            {
+                Console.WriteLine("Action3 " + obj);
+                Console.WriteLine("Action3 " + obj + " Naber");
+            });
+            action3.Invoke("Ali");
+
+            Action action4 = new Action(() =>
+            {
+                Console.WriteLine("ali veli");
+            });
+            action4.Invoke();
+
+            Console.ReadKey();
+        }
+
+        private static void SelamVer2(string obj)
+        {
+            Console.WriteLine(obj + " Selam");
+        }
+
+        private static void SelamVer()
+        {
+            Console.WriteLine("Selam Verdim");
+        }
+    }
+}
